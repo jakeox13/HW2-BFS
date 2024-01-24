d6ea718fe6113487ab69a0a9e8c7c368791333cb
jakeox13 <jakeox13@gmail.com>
2024-01-24 14:07:29 -0800

    "First commit test"

--- search/graph.py
+++ search/graph.py
@@ -1,5 +1,5 @@
 import networkx as nx
-
+from collections import deque
 class Graph:
     """
     Class to contain a graph and your bfs function
@@ -21,8 +21,56 @@ class Graph:
         * If there is an end node input and a path does not exist, return None
 
         """
-        return
+        # Check to make sure inputs are valid
+
+        # Check for empty graph
+        if  len(list(self.graph.nodes))==0:
+            raise ValueError("This graph is empty")
+        # Check for vaild starting point
+        if start not in list(self.graph.nodes):
+            raise ValueError("Starting node is not present in graph")
+        # Check for vaild end if one is given
+        if end != None and end not in list(self.graph.nodes):
+            raise ValueError("Ending node is not present in graph")
+        
+        # Intialize the queue object
+        Q= deque()
+
+        # Initilaize the list of nodes visited 
+        visited=[start]
+        
+        # Add starting node to queue
+        Q.append(start)
+        
+        end_found=False
+        # Create a dictionary of paths taken to each node
+        path= dict()
+        # Add the start to the path of all others
+        path[start]=[start]
+        # Loop through node
+        while Q and not end_found:
+            # Get first value in queue
+            v=Q.popleft()
+            # get neighbors of node you are investigating
+            n = self.graph.adj[v]
+
+            # Check if each neighbor has been visted if visted pass
+            for w in n:
+                if w not in visited:
+                    # If not visited add to queue and add to visted list
+                    visited.append(w)
+                    Q.append(w)
+                    # Set the path of current to path of previous + current
+                    if end != None:
+                        path[w]=path[v]+[w]
+                    # If this is end node stop searching
+                    if w == end:
+                        end_found= True
 
 
-
-
+        if end == None:
+            return visited
+        elif end_found == False:
+            return None
+        else:
+            return path[end]
--- test/test_bfs.py
+++ test/test_bfs.py
@@ -1,26 +1,40 @@
 # write tests for bfs
 import pytest
-from search import graph
+from search import Graph
+import networkx as nx
+from collections import deque
+
+
 
 def test_bfs_traversal():
     """
-    TODO: Write your unit test for a breadth-first
-    traversal here. Create an instance of your Graph class 
-    using the 'tiny_network.adjlist' file and assert 
-    that all nodes are being traversed (ie. returns 
-    the right number of nodes, in the right order, etc.)
+    Unit tests for a breadth-first
+    traversal using the 'tiny_network.adjlist'
     """
+    # Read in graph
+    test=Graph("data/tiny_network.adjlist")
+
+    # Check to make sure the output is what is expected
+    assert test.bfs('Michael Keiser') == ['Michael Keiser', '33232663', 'Charles Chiu', 'Martin Kampmann', '33242416', '33483487', '32790644', '31806696', '31626775', '31540829', 'Atul Butte', 'Luke Gilbert', 'Steven Altschuler', 'Lani Wu', 'Neil Risch', 'Nevan Krogan', '33765435', '31395880', '30944313', '32036252', '32042149', '30727954', '29700475', '34272374', '32353859', 'Marina Sirota', 'Hani Goodarzi', 'Michael McManus', '31486345', '32025019']
+    assert test.bfs('Lani Wu') == ['Lani Wu', '32042149', '32036252', '31806696', '30727954', 'Hani Goodarzi', 'Steven Altschuler', 'Luke Gilbert', 'Michael McManus', '33232663', '33483487', '31626775', '31540829', '32025019', '29700475', 'Charles Chiu', 'Martin Kampmann', 'Neil Risch', 'Nevan Krogan', 'Atul Butte', 'Michael Keiser', '33242416', '32790644', '34272374', '32353859', '30944313', '33765435', '31395880', 'Marina Sirota', '31486345']
+    
+    with pytest.raises(ValueError, match= r"Starting node is not present in graph"):
+        test.bfs('Jake Oxendine')
     pass
 
 def test_bfs():
     """
-    TODO: Write your unit test for your breadth-first 
-    search here. You should generate an instance of a Graph
-    class using the 'citation_network.adjlist' file 
-    and assert that nodes that are connected return 
-    a (shortest) path between them.
-    
-    Include an additional test for nodes that are not connected 
-    which should return None. 
+    Unit test fora breadth-first 
+    using the 'citation_network.adjlist' 
     """
-    pass
+    test=Graph("data/citation_network.adjlist")
+    # Test correct implimentation
+    assert test.bfs("Michael Keiser","Atul Butte") == ['Michael Keiser', '33232663', 'Charles Chiu', '33242416', 'Atul Butte']
+    
+    # Test missing ending node implimentation
+    with pytest.raises(ValueError, match= r"Ending node is not present in graph"):
+        test.bfs('Marina Sirota','Jake Oxendine')
+
+    # Test not connecting nodes return 1
+    assert test.bfs('Marina Sirota','Alexander (Sandy) Johnson') == None
+
